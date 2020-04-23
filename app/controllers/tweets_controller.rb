b74a22b3e73cf465f7246db256c77413e6417b89
asaki kusano <darumade4@gmail.com>
2020-04-23 18:39:49 +0900

    createアクション作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,5 +3,16 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to new_tweet_path
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title,:content)
   end
 end
